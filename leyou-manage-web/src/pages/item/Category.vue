440f17601e383975bf7dfc710ae19355fe611727
ONAMHE <114335455+ONAMHE@users.noreply.github.com>
2023-03-12 22:36:59 +0800

    leyou.portal的初始版本

---
+++ leyou-manage-web/src/pages/item/Category.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card>
+      <v-flex xs12 sm10>
+        <!--                如果使用远程调用数据那么就不能有楼下的代码模型否者就会调用楼下的-->
+        <!--                :treeData="treeData"-->
+        <v-tree url="/item/category/list"
+                :isEdit="isEdit"
+                @handleAdd="handleAdd"
+                @handleEdit="handleEdit"
+                @handleDelete="handleDelete"
+                @handleClick="handleClick"
+        />
+      </v-flex>
+  </v-card>
+</template>
+
+<script>
+  import {treeData} from "../../mockDB";
+
+  export default {
+    name: "category",
+    data() {
+      return {
+        isEdit:true,
+        treeData
+      }
+    },
+    methods: {
+      handleAdd(node) {
+        console.log("add .... ");
+        console.log(node);
+      },
+      handleEdit(id, name) {
+        console.log("edit... id: " + id + ", name: " + name)
+      },
+      handleDelete(id) {
+        console.log("delete ... " + id)
+      },
+      handleClick(node) {
+        console.log(node)
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
